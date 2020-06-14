c32da628550f00f83b35915b4a54a5588d69422b
ManojKorrapati <57298769+ManojKorrapati@users.noreply.github.com>
2020-06-14 17:12:30 -0400

    Update hook.py

--- hook.py
+++ hook.py
@@ -1,46 +1,46 @@
 from app.objects.c_obfuscator import Obfuscator
 from app.utility.base_world import BaseWorld
-from plugins.robinhood.app.robinhood_svc import RobinhoodService
+from plugins.secorg.app.secorg_svc import RobinhoodService
 
-name = 'Robinhood'
+name = 'Secorg'
 description = 'A stockpile of abilities, adversaries, payloads and planners'
-address = '/plugin/robinhood/gui'
+address = '/plugin/secorg/gui'
 access = BaseWorld.Access.RED
 
 
 async def enable(services):
-    robinhood_svc = RobinhoodService(services)
-    services.get('app_svc').application.router.add_route('GET', '/plugin/robinhood/gui', robinhood_svc.splash)
-    await services.get('file_svc').add_special_payload('.donut', 'plugins.robinhood.app.donut.donut_handler')
-    await robinhood_svc.data_svc.store(
+    secorg_svc = SecorgService(services)
+    services.get('app_svc').application.router.add_route('GET', '/plugin/secorg/gui', secorg_svc.splash)
+    await services.get('file_svc').add_special_payload('.donut', 'plugins.secorg.app.donut.donut_handler')
+    await secorg_svc.data_svc.store(
         Obfuscator(name='plain-text',
                    description='Does no obfuscation to any command, instead running it in plain text',
-                   module='plugins.robinhood.app.obfuscators.plain_text')
+                   module='plugins.secorg.app.obfuscators.plain_text')
     )
-    await robinhood_svc.data_svc.store(
+    await secorg_svc.data_svc.store(
         Obfuscator(name='base64',
                    description='Obfuscates commands in base64',
-                   module='plugins.robinhood.app.obfuscators.base64_basic')
+                   module='plugins.secorg.app.obfuscators.base64_basic')
     )
-    await robinhood_svc.data_svc.store(
+    await secorg_svc.data_svc.store(
         Obfuscator(name='base64jumble',
                    description='Obfuscates commands in base64, then adds characters to evade base64 detection. '
                                'Disclaimer: this may cause duplicate links to run.',
-                   module='plugins.robinhood.app.obfuscators.base64_jumble')
+                   module='plugins.secorg.app.obfuscators.base64_jumble')
     )
-    await robinhood_svc.data_svc.store(
+    await secorg_svc.data_svc.store(
         Obfuscator(name='caesar cipher',
                    description='Obfuscates commands through a caesar cipher algorithm, which uses a randomly selected '
                                'shift value.',
-                   module='plugins.robinhood.app.obfuscators.caesar_cipher')
+                   module='plugins.secorg.app.obfuscators.caesar_cipher')
     )
-    await robinhood_svc.data_svc.store(
+    await secorg_svc.data_svc.store(
         Obfuscator(name='base64noPadding',
                    description='Obfuscates commands in base64, then removes padding',
-                   module='plugins.robinhood.app.obfuscators.base64_no_padding')
+                   module='plugins.secorg.app.obfuscators.base64_no_padding')
     )
-    await robinhood_svc.data_svc.store(
+    await secorg_svc.data_svc.store(
         Obfuscator(name='steganography',
                    description='Obfuscates commands through image-based steganography',
-                   module='plugins.robinhood.app.obfuscators.steganography')
+                   module='plugins.secorg.app.obfuscators.steganography')
     )
